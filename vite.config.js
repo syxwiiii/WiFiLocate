34f9800a5b39e47c69fbdf842b40ffacdc9a37e9
syxwiiii <syxmwiinikov@list.ru>
2025-05-10 10:00:48 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,10 +5,11 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: '0.0.0.0',
+    host: '0.0.0.0', // Позволяет доступ по локальному IP
   },
   build: {
     rollupOptions: {
+      // Указываем файл как внешнюю зависимость
       external: ['/src/index.jsx']
     }
   }
