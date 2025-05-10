6eef9a91c9ad8f3e34d012340269550bfcf5ee95
syxwiiii <syxmwiinikov@list.ru>
2025-05-10 07:57:15 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import App from './src/App';
 
 export default defineConfig({
   plugins: [react()],
-  base: './WiFiLocate/', // Указывает, что пути должны быть относительными
-  server: {
-    host: '0.0.0.0',
+  build: {
+    cssCodeSplit: false, // Объединить CSS в один файл
+    outDir: 'dist', // Укажите выходную директорию
   },
 });
