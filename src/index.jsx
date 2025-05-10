8b18c79a5a41c1e69cc528ea4a8afd8f1b301ea6
syxwiiii <syxmwiinikov@list.ru>
2025-05-10 08:07:14 -0300

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './src/'
+import { Map } from '@pbe/react-yandex-maps';
+import axios from 'axios';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
