e29dab4b6b8b654d4eddfc0efb322153e0266af4
syxwiiii <syxmwiinikov@list.ru>
2025-05-10 07:59:55 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import App from './src/App';
+import './App.css';
 
 export default defineConfig({
   plugins: [react()],
